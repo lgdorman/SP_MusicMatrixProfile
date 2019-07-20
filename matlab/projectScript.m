705b50ac223b94f81250b2b9a510756caea8b527
Logan <ldorman2012@gmail.com>
2019-07-20 09:48:08 -0400

    Load entire sonata audio

--- matlab/projectScript.m
+++ matlab/projectScript.m
@@ -15,12 +15,14 @@ rondoFilename = '..\audioFiles\IMSLP111573-PMLP01846-Música_Clásica_-_Rondo_Alla
 % two columns of a are left and right channels. For simplicity, work only
 % with left channel a(:,1);
 
+downSample = 100;
+
+
 
 %%
 % shorten data for intial analysis
 startInd = find(a(:,1),1); % find first non-zero index
 
-downSample = 10;
 
 startDat = a(startInd:downSample:floor(length(a)/4),1);
 
@@ -41,3 +43,37 @@ title({'Matrix Profile'; 'Mozart Piano Sonata No. 11 in A Maj'; '(First 50 secon
 figure; plot(time, time(profileIndex))
 title({'Profile Index'; 'Mozart Piano Sonata No. 11 in A Maj'; '(First 50 seconds)'}); grid on;
 xlabel('Time of Section'); ylabel('Time of Most Similar Section')
+
+%%
+% look at entire audio with very large seg length
+
+downA = a(1:downSample:end,1);
+longSegLen = floor(length(downA)/20);
+
+[matrixProfile, profileIndex, motifIdxs, discordIdx] = interactiveMatrixProfileVer3_website(downA, longSegLen);
+
+figure; plot(profileIndex)
+
+%%
+a_10Hz = resample(a(:,1), 10, f); % downsample from file rate to 10Hz
+segLen_10Hz = 200; % 20 second segments
+
+[matrixProfile_10Hz, profileIndex_10Hz, motifIdxs_10Hz, discordIdx_10Hz] = interactiveMatrixProfileVer3_website(a_10Hz, segLen_10Hz);
+
+save('.\MozartSonata_10Hz_SegLen200.mat', 'matrixProfile_10Hz', 'profileIndex_10Hz');
+
+%%
+
+time = (0:length(a_10Hz)-segLen_10Hz)/10;
+figure; plot(time, profileIndex_10Hz/10);
+xlim([time(1) time(end)])
+
+
+time = (0:length(a_10Hz)-segLen_10Hz)/10;
+figure; plot(time, matrixProfile_10Hz);
+xlim([time(1) time(end)])
+
+%%
+
+
+
